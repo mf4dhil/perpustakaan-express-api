e4c3ce3a00ab3b1b1e3f90c2add4ec84785eb887
Fadhil0109 <syafwah999@gmail.com>
2023-02-12 14:07:40 +0700

    Menambahkan create book pada Book.controller.js

--- controller/Book.controller.js
+++ controller/Book.controller.js
@@ -1,3 +1,4 @@
+import { Op } from "sequelize"
 import Books from "../models/Book.model.js"
 import BookCategorys from "../models/BookCategory.model.js"
 import BookGenres from "../models/BookGenre.model.js"
@@ -83,8 +84,81 @@ export const getBookById = async (req, res) => {
   }
 }
 export const createBook = async (req, res) => {
-  const { name, author, release_date } = req.body
-  
+  const { name, author, release_date, genre1, genre2, genre3, category1, category2 } = req.body
+  const gnr = await Genres.findAll({
+    where: {
+      [Op.or]: [{name: genre1},{name: genre2},{name: genre3},]
+    }
+  })
+  const ctgr = await Categorys.findAll({
+    where: {
+      [Op.or]: [{name: category1}, {name: category2}]
+    }
+  })
+  try {
+    await Books.create(
+      {
+        name: name,
+        author: author,
+        release_date: release_date
+      }
+    )
+    const book = await Books.findOne({
+      where: {
+        [Op.and]: [{name: name}, {author: author}, {release_date: release_date}]
+      }
+    })
+    if( !book ) return res.status(404).json({msg: "Book tidak ditemukan"})
+    if( !gnr[0] && !gnr[1] ) return res.status(400).json({msg: "Minimal terdapat Dua genre!"})
+    if( !ctgr[0] && !ctgr[1] ) return res.status(400).json({msg: "Minimal terdapat Dua Category!"})
+    if( !gnr[2] ) {
+      await BookGenres.bulkCreate([
+        {
+          ID_book: book.id,
+          ID_genre: gnr[0].id
+        },
+        {
+          ID_book: book.id,
+          ID_genre: gnr[1].id
+        }
+      ])
+    } else {
+      await BookGenres.bulkCreate([
+        {
+          ID_book: book.id,
+          ID_genre: gnr[0].id
+        },
+        {
+          ID_book: book.id,
+          ID_genre: gnr[1].id
+        },
+        {
+          ID_book: book.id,
+          ID_genre: gnr[2].id
+        }
+      ])
+    }
+    if( !ctgr[1] ) {
+      await BookCategorys.create({
+        ID_book: book.id,
+        ID_category: ctgr[0].id
+      })
+    } else {
+      await BookCategorys.bulkCreate([
+        {
+          ID_book: book.id,
+          ID_category: ctgr[0].id
+        },
+        {
+          ID_book: book.id,
+          ID_category: ctgr[1].id
+        }
+      ])
+    }
+    res.status(201).json({msg: "Books Created Successfully!"})
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
 }
 export const updateBook = async (req, res) => {
 
--- routes/Book.routes.js
+++ routes/Book.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {} from "../controller/Book.controller.js"
+import { createBook, getBook } from "../controller/Book.controller.js"
 
 const router = express.Router()
 
-router.get('/books', )
+router.get('/books', getBook)
+router.post('/books', createBook)
 
 export default router
